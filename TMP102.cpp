68c980385961efd36b9c10447b23a1a8575fb0d2
John Larkin <jlarkin@whitworth.edu>
2020-02-19 14:25:05 -0800

    Add configuration options

--- TMP102.cpp
+++ TMP102.cpp
@@ -24,30 +24,79 @@ THE SOFTWARE.
 #include "TMP102.h"
 
 #define TEMP_REG_ADDR 0x00
+#define CONFIG_REG_ADDR 0x01
+#define TLOW_REG_ADDR 0x02
+#define THIGH_REG_ADDR 0x03
 
-TMP102::TMP102(I2C i2c, int addr) : m_i2c(i2c), m_addr(addr)
+TMP102::TMP102(I2C& i2c, int addr7bit) : _i2c(i2c)
 {
-
+  _addr = addr7bit << 1;
+  _config[0] = 0x60; // Default value of first config byte
+  _config[1] = 0xA0; // Default value of second config byte
 }
 
 TMP102::~TMP102()
 {
-
 }
 
 float TMP102::read()
 {
+  char buf[2];
+  int16_t rawBits;
   
-  const char tempRegAddr = TEMP_REG_ADDR;
+  buf[0] = TEMP_REG_ADDR;     // Set pointer to Temperature Register
+  _i2c.write(_addr, buf, 1);
+  _i2c.read(_addr, buf, 2);   // Read two bytes from register
 
-  m_i2c.write(m_addr, &tempRegAddr, 1); //Pointer to the temperature register
- 
-  char reg[2] = {0,0};
-  m_i2c.read(m_addr, reg, 2); //Rea
-  
-  int16_t res  = ((int8_t)reg[0] << 4) | ((uint8_t)reg[1] >> 4);  
-  
-  float temp =  (float) ((float)res * 0.0625);
-   
-  return temp;
+  rawBits = (buf[0] << 8) | buf[1]; // Combine both bytes
+  // Detect whether in normal temperature mode or extended temperature mode. Bit 0 flags this mode.
+  if (buf[1] & 1) { // Extended Temperature Mode flag set
+    rawBits = rawBits >> 3;
+  } else {
+    rawBits = rawBits >> 4;
+  }
+     
+  return (float) ((float)rawBits * 0.0625);
+}
+
+void TMP102::config_ext_temp_mode(bool enable) {
+  char buf[3];
+  buf[0] = CONFIG_REG_ADDR;
+  buf[1] = _config[0];
+  if (enable) { // 13-bit mode, so technically up to 256 deg C but PCB may fail well before then
+    _config[1] |= (1 << 5); // Turn on bit 5
+  } else { // 12-bit mode, so max temp of 127.9375 deg C
+    _config[1] &= ~(1 << 5); // Turn off bit 5
+  }
+  buf[2] = _config[1];
+  _i2c.write(_addr, buf, 3);
+}
+
+void TMP102::config_conversion_rate(TMP102_ConversionRate_e newRate) {
+  char buf[3];
+  buf[0] = CONFIG_REG_ADDR;
+  buf[1] = _config[0];
+  switch (newRate) {
+    case CONVERT_RATE_QUARTER_HZ:
+      _config[1] &= ~(1 << 7); // Turn off bit 7
+      _config[1] &= ~(1 << 6); // Turn off bit 6
+      break;
+    case CONVERT_RATE_1_HZ:
+      _config[1] &= ~(1 << 7); // Turn off bit 7
+      _config[1] |= (1 << 6);  // Turn on bit 6
+      break;
+    case CONVERT_RATE_4_HZ:
+      _config[1] |= (1 << 7); // Turn on bit 7
+      _config[1] &= ~(1 << 6);  // Turn off bit 6
+      break;
+    case CONVERT_RATE_8_HZ:
+      _config[1] |= (1 << 7); // Turn on bit 7
+      _config[1] |= (1 << 6); // Turn on bit 6
+      break;
+    default:
+      // No change to parameters if invalid code received
+      break;
+  }
+  buf[2] = _config[1];
+  _i2c.write(_addr, buf, 3);
 }
--- TMP102.h
+++ TMP102.h
@@ -1,61 +1,73 @@
-
-/*
-Copyright (c) 2010 Donatien Garnier (donatiengar [at] gmail [dot] com)
- 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
- 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
- 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
+/*****************************************************************************
+ * TMP102 Temperature Sensor Class
+ * 
+ * Developed by John M. Larkin, Whitworth University
+ * 
+ * MIT License
+ * Copyright (c) 2020 John Larkin
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ ****************************************************************************/
 
 #ifndef TMP102_H
 #define TMP102_H
 
-#include "mbed.h"
+#include <mbed.h>
 
-//!Library for the TI TMP102 temperature sensor.
-/*!
-The TMP102 is an I2C digital temperature sensor in a small SOT563 package, with a 0.0625C resolution and 0.5C accuracy.
-*/
+enum TMP102_ConversionRate_e {
+  CONVERT_RATE_QUARTER_HZ,
+  CONVERT_RATE_1_HZ,
+  CONVERT_RATE_4_HZ,
+  CONVERT_RATE_8_HZ
+};
+
 class TMP102
 {
 public:
-  //!Creates an instance of the class.
-  /*!
-  Connect module at I2C address addr using I2C port pins sda and scl.
-  TMP102
-  \param addr <table><tr><th>A0 pin connection</th><th>Address</th></tr><tr><td>GND</td><td>0x90</td></tr><tr><td>V+</td><td>0x92</td></tr><tr><td>SDA</td><td>0x94</td></tr><tr><td>SCL</td><td>0x96</td></tr></table>  
-  */
-  TMP102(I2C i2c, int addr);
+  // Creates an instance of the class.
+  // Connect module using a shared I2C object
+  //
+  // The 7-bit I2C address is selected using pin ADD0.
+  //   ADD0 to ground = 0x48
+  //   ADD0 to VCC = 0x49
+  //   ADD0 to SDA = 0x4A
+  //   ADD0 to SCL = 0x4B
+  //
+  // On the SparkFun breakout board, ADD0 comes connected to ground via a jumper on the board so the default address is 0x48.
+  // If you want a different address, this jumper must be cut and ADD0 connected appropriately.  
+  TMP102(I2C& i2c, int addr7bit = 0x48);
   
-  /*!
-  Destroys instance.
-  */ 
   ~TMP102();
   
-  //!Reads the current temperature.
-  /*!
-  Reads the temperature register of the TMP102 and converts it to a useable value.
-  */
+  // Reads the current temperature, autodetecting 12-bit or 13-bit temperature mode
   float read();
+
+  // Set temperature range to extended (up to 256 deg C) mode rather than standard (up to 128 deg C)
+  void config_ext_temp_mode(bool enable);
+
+  // Set rate of temperature conversions if in continuous conversion mode
+  void config_conversion_rate(TMP102_ConversionRate_e newRate);
   
 private:
-  I2C m_i2c;
-  int m_addr;
-
+  I2C& _i2c;
+  int _addr; // store address in 8-bit form
+  char _config[2]; // configuration bytes
 };
 
 #endif
